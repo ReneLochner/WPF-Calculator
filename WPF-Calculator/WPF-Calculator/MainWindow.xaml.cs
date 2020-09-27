6b4546bbfde9e326a531da6caa731e7fd0e50d14
Rene Lochner <school@renelochner.at>
2020-09-27 10:59:01 +0200

    Implement Logic

--- WPF-Calculator/WPF-Calculator/MainWindow.xaml.cs
+++ WPF-Calculator/WPF-Calculator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace WPF_Calculator
 {
@@ -20,9 +21,63 @@ namespace WPF_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly string CLEAR = "C";
+        readonly string RESULT = "=";
+        readonly string ZERO = "0";
+
         public MainWindow()
         {
             InitializeComponent();
+            btnNumZero.Click += InputButton_Click;
+            btnNumOne.Click += InputButton_Click;
+            btnNumTwo.Click += InputButton_Click;
+            btnNumThree.Click += InputButton_Click;
+            btnNumFour.Click += InputButton_Click;
+            btnNumFive.Click += InputButton_Click;
+            btnNumSix.Click += InputButton_Click;
+            btnNumSeven.Click += InputButton_Click;
+            btnNumEight.Click += InputButton_Click;
+            btnNumNine.Click += InputButton_Click;
+            btnOpDivi.Click += InputButton_Click;
+            btnOpMulti.Click += InputButton_Click;
+            btnOpPlus.Click += InputButton_Click;
+            btnOpMinus.Click += InputButton_Click;
+            btnOpClear.Click += InputButton_Click;
+            btnOpRes.Click += InputButton_Click;
+        }
+
+        private void InputButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button pressedButton = (Button)sender;
+            string buttonContent = pressedButton.Content.ToString();
+
+            if(buttonContent.Equals(CLEAR)) {
+                textblockContent.Text = ZERO;
+                return;
+            }
+            else if(buttonContent.Equals(RESULT))
+            {
+                DataTable dataTable = new DataTable();
+                try
+                {
+                    string result = dataTable
+                        .Compute(textblockContent.Text, null)
+                        .ToString();
+                    textblockContent.Text = result;
+                }
+                catch(SyntaxErrorException exc)
+                {
+                    MessageBox.Show(exc.Message);
+                    textblockContent.Text = ZERO;
+                }
+                return;
+            }
+
+            string calculatorString = textblockContent.Text;
+
+            calculatorString = calculatorString.TrimStart('0');
+            calculatorString += buttonContent;
+            textblockContent.Text = calculatorString;
         }
     }
 }
